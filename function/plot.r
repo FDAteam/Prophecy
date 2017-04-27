e2723cc9624aff034ae10e61a19a02b10e30e81e
Bassem16 <bassem.gharbi.92@gmail.com>
2017-04-27 14:43:21 +0200

    Ajout de fonction de ploting (backtest + ratio) dans un fichier séparé

---
+++ function/plot.r
@@ -0,0 +1,53 @@
+
+
+
+
+
+backtest.plot<-function(df.bcp){
+  
+  p<-plot_ly(
+    
+    data = data,
+    x = ~date,
+    y = ~val,
+    name = 'Market',
+    type = 'scatter',
+    mode = 'lines',
+    colors = "Dark2",
+    connectgaps = TRUE
+  )%>%
+    add_trace(y = df.bcp[,12,drop=T], name = 'Bayesian Investor', mode = 'lines')
+  
+  data = data.frame(date=index(x), val=df.bcp[,8,drop=T])
+  q<-plot_ly(
+    
+    data = data,
+    x = ~date,
+    y = ~val,
+    name = 'Bayesian Sharpe',
+    type = 'scatter',
+    mode = 'lines',
+    colors = "Dark2",
+    connectgaps = TRUE
+  )%>%
+    add_trace(y = df.bcp[,9,drop=T], name = 'Threshold', mode = 'lines')
+  
+  subplot(p,q, nrows = 2, shareX = TRUE) %>% layout(legend = list(orientation = 'h'))
+}
+
+
+ratio.plot<-function(df.bcp){  
+  
+  data = data.frame(date=index(df.bcp), val=df.bcp[,8,drop=T])
+  plot_ly(
+    
+    data = data,
+    x = ~date,
+    y = ~val,
+    name = 'Bayesian Sharpe',
+    type = 'scatter',
+    mode = 'lines',
+    connectgaps = TRUE
+  )%>%
+    add_trace(y = df.bcp[,9,drop=T], name = 'Threshold', mode = 'lines')
+}
--- main.r
+++ main.r
@@ -1,68 +1,6 @@
 source("function/global.r")
-
+source("function/global.r")
 #tout les plots ici
-backtest.plot<-function(df.bcp){
-
-    p<-plot_ly(
-      
-      data = data,
-      x = ~date,
-      y = ~val,
-      name = 'Market',
-      type = 'scatter',
-      mode = 'lines',
-      colors = "Dark2",
-      connectgaps = TRUE
-    )%>%
-      add_trace(y = df.bcp[,12,drop=T], name = 'Bayesian Investor', mode = 'lines')
-    
-    data = data.frame(date=index(x), val=df.bcp[,8,drop=T])
-    q<-plot_ly(
-      
-      data = data,
-      x = ~date,
-      y = ~val,
-      name = 'Bayesian Sharpe',
-      type = 'scatter',
-      mode = 'lines',
-      colors = "Dark2",
-      connectgaps = TRUE
-    )%>%
-      add_trace(y = df.bcp[,9,drop=T], name = 'Threshold', mode = 'lines')
-    
-    subplot(p,q, nrows = 2, shareX = TRUE) %>% layout(legend = list(orientation = 'h'))
-  })
-
-
-ratio.plot<-function(df.bcp){  
-
-    data = data.frame(date=index(df.bcp), val=df.bcp[,8,drop=T])
-    plot_ly(
-      
-      data = data,
-      x = ~date,
-      y = ~val,
-      name = 'Bayesian Sharpe',
-      type = 'scatter',
-      mode = 'lines',
-      connectgaps = TRUE
-    )%>%
-      add_trace(y = df.bcp[,9,drop=T], name = 'Threshold', mode = 'lines')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     # Same as above, but with fill=TRUE
 
