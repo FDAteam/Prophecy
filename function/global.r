f7fc5c02c2a043aab4b4d7cd871b376f32a95b7e
niak12 <niakhub@gmail.com>
2017-04-30 19:02:14 +0200

    commit nika12 branvhe

--- function/global.r
+++ function/global.r
@@ -25,7 +25,7 @@ bcp.investor <- function(DataSet,assetsName, lag, sensibility){
   
   #asset<-getSymbols(assetsName,auto.assign = FALSE)
   #wkreturn<- weeklyReturn(asset,auto.assign = FALSE)-0.004
-  
+  message("test branche Niak2")
   
   #asset<-getSymbols(assetsName,auto.assign = FALSE)
   
